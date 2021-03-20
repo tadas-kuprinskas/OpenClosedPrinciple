628ca1f06720b9d4c5c5781a938c62e8deed1f66
iltoeeai <tadas.kuprinskas@gmail.com>
2021-03-20 23:55:26 +0200

    initial commit

---
+++ OCPLibrary/Accounts/Accounts.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace OCPLibrary
+{
+    public class Accounts : IAccounts
+    {
+        public EmployeeModel Create(IApplicantModel person)
+        {
+            EmployeeModel output = new EmployeeModel();
+
+            output.FirstName = person.FirstName;
+            output.LastName = person.LastName;
+            output.EmailAddress = $"{person.FirstName.Substring(0, 1)}{person.LastName}@test.com";
+
+
+            return output;
+        }
+    }
+}
---
+++ OpenClosedPrinciple/Program.cs
@@ -0,0 +1,32 @@
+﻿using OCPLibrary;
+using System;
+using System.Collections.Generic;
+
+namespace OpenClosedPrinciple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<IApplicantModel> applicants = new List<IApplicantModel>
+            {
+            new PersonModel {FirstName = "John", LastName = "Johnson" },
+            new ManagerModel {FirstName = "Sue", LastName = "Samson" },
+            new ExecutiveModel {FirstName = "Willy", LastName = "Wilson" },
+            };
+
+            List<EmployeeModel> employees = new List<EmployeeModel>();
+            
+
+            foreach (var person in applicants)
+            {
+                employees.Add(person.AccountProcessor.Create(person));
+            }
+
+            foreach (var emp in employees)
+            {
+                Console.WriteLine($"{emp.FirstName} {emp.LastName}: {emp.EmailAddress}, Is Manager: {emp.IsManager}, Is Executive: {emp.IsExecutive} ");
+            }
+        }
+    }
+}
